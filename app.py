3ecb087804b732879d4e8f198d7cd722e7958317
stephen <stephenfanale@hotmail.com>
2018-10-25 12:06:57 -0400

    -- hello world page

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import (Flask, render_template )
 
-app = Flask(__name__)
+# create application instance
+app = Flask(__name__, template_folder="templates")
 
-
+# create a url for application at /
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+def home():
+    """
+        This function just responds to the browser ULR
+        localhost:5000/
 
+        :return:        the rendered template 'home.html'
+        """
+    return render_template("home.html")
+
+# if we are running in stand alone mode, run the application
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
