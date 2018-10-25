0897711d1affb9617ae6038231cb861ff1929dc8
stephen <stephenfanale@hotmail.com>
2018-10-25 12:16:58 -0400

    -- reconfigured app to use connextion -- added swagger.yml file to configure api endpoints

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import (Flask, render_template )
+import connexion
 
 # create application instance
-app = Flask(__name__, template_folder="templates")
+app = connexion.App(__name__, specification_dir="./")
+
+# read swagger.yml file to configure endpoints
+app.add_api('swagger.yml')
+
 
 # create a url for application at /
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
 
 def home():
     """
@@ -17,6 +23,7 @@ def home():
         """
     return render_template("home.html")
 
+
 # if we are running in stand alone mode, run the application
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
